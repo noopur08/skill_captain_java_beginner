022cceac215c714ef0aa562c39436f58c0724599
Noopur <f20221245@goa.bits-pilani.ac.in>
2023-06-11 21:06:18 +0530

    day two

---
+++ DataType.java
@@ -0,0 +1,35 @@
+public class DataType {
+    public static void main(String[] args) {
+
+        // boolean
+        boolean bool = true;
+
+        // character data type
+        char char1 = 'N';
+
+        //integer data type
+        byte byte1 = 1;
+        short short1 = 12;
+        int int1 = 500;
+        long long1 = 6257L;
+
+        // floating data type
+        float float1 = 2.982F;
+        double double1 = 231.231;
+        
+        // string data type
+        String string1 = "assignment 2";
+
+
+        // printing the values
+        System.out.println("Boolean value: " + bool);
+        System.out.println("Character value: " + char1);
+        System.out.println("Byte value: " + byte1);
+        System.out.println("Short value: " + short1);
+        System.out.println("Int value: " + int1);
+        System.out.println("Long value: " + long1);
+        System.out.println("Float value: " + float1);
+        System.out.println("Double value: " + double1);
+        System.out.println("String value: " + string1);
+    }
+}
