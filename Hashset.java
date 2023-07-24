76cb74da022b9c82b816fab18b4d746339500cbc
Noopur <f20221245@goa.bits-pilani.ac.in>
2023-07-24 17:54:42 +0530

    day ten

---
+++ Hashset.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Hashset {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Unique Number Checker:");
+        System.out.print("Enter a series of numbers (separated by spaces): ");
+        String input = scanner.nextLine();
+
+        String[] numberStrings = input.split("\\s+");
+
+        Set<Integer> uniqueNumbers = new HashSet<>();
+
+        System.out.println("\nNumber\t\tStatus");
+        System.out.println("----------------------");
+
+        for (String numberString : numberStrings) {
+            int number = Integer.parseInt(numberString);
+
+            if (uniqueNumbers.contains(number)) {
+                System.out.println(number + "\t\tDuplicate");
+            } else {
+                uniqueNumbers.add(number);
+                System.out.println(number + "\t\tUnique");
+            }
+        }
+
+        scanner.close();
+    }
+}
